da510b1e00859f8ed8cdda9c2d0b0f785bdb4c7a
Eryk07 <errryk333@gmail.com>
2019-12-28 17:49:22 +0100

    corrected first model

--- dependencies.py
+++ dependencies.py
@@ -1,13 +1,15 @@
-import pandas as pd
 import numpy as np
 import os
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix
 import keras
-import matplotlib.pyplot as plt
+from keras.preprocessing.image import load_img, img_to_array, ImageDataGenerator
+from keras.utils import to_categorical
 from keras.layers import Dense,GlobalAveragePooling2D
 from keras.applications import MobileNet
-from keras.preprocessing import image
-from keras.applications.mobilenet import preprocess_input
-from keras.preprocessing.image import ImageDataGenerator
 from keras.models import Model
 from keras.optimizers import Adam
-
+from keras.metrics import accuracy
+from PIL import Image
+from IPython.display import display
+from IPython.display import Image as _Imgdis
